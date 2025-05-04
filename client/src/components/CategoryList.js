4ffb5d83f7bc63627c2a80ba0e096203b2a7d685
Luka Baranovic <lukabaranovic366@gmail.com>
2025-05-05 00:13:53 +0200

    save

--- client/src/components/CategoryList.js
+++ client/src/components/CategoryList.js
@@ -3,7 +3,6 @@ import "./CategoryList.css"; // Import the CSS file
 import CategoryPopup from "./CategoryPopup.js"; // Import the popup component
 
 const CategoryList = ({ categories, addItemToCart }) => {
-  // Add addItemToCart to props
   const [selectedCategory, setSelectedCategory] = useState(null);
 
   const handleCategoryClick = (category) => {
@@ -15,24 +14,23 @@ const CategoryList = ({ categories, addItemToCart }) => {
   };
 
   return (
-    <div>
-      <ul>
-        {categories.map((category) => (
-          <li
-            key={category.category_id}
-            className="category-container"
-            onClick={() => handleCategoryClick(category)}
-          >
-            {category.category_name}
-          </li>
-        ))}
-      </ul>
+    <div className="category-list">
+      {categories.map((category) => (
+        <div
+          key={category.category_id}
+          className="category-container"
+          onClick={() => handleCategoryClick(category)}
+        >
+          {category.category_name}
+        </div>
+      ))}
+
       {selectedCategory && (
         <CategoryPopup
           categoryId={selectedCategory.category_id}
           categoryName={selectedCategory.category_name}
           onClose={handleClosePopup}
-          addItemToCart={addItemToCart} // Pass addItemToCart to CategoryPopup
+          addItemToCart={addItemToCart}
         />
       )}
     </div>
