4cc20da1d888884d22809e83ee2be9546cda5dcb
Luka Baranovic <lukabaranovic366@gmail.com>
2025-05-06 12:51:44 +0200

    12:02

--- client/src/components/CategoryPopup.js
+++ client/src/components/CategoryPopup.js
@@ -7,7 +7,6 @@ const CategoryPopup = ({
   onClose,
   addItemToCart,
 }) => {
-  // Add addItemToCart to props
   const [items, setItems] = useState([]);
 
   useEffect(() => {
@@ -19,25 +18,32 @@ const CategoryPopup = ({
   return (
     <div className="popup-overlay">
       <div className="popup-content">
-        <h2>{categoryName}</h2>
-        <hr className="smooth-line" />
-        {items.length > 0 ? (
-          <ul className="item-list">
-            {items.map((item) => (
-              <li key={item.item_id} className="item-container">
-                <p className="item-name">{item.item_name}</p>
-                <p className="item-price">€{item.item_price}</p>
-                <button onClick={() => addItemToCart(item)}>
-                  Add to Cart
-                </button>{" "}
-                {/* Add button to add items to cart */}
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No items found for this category.</p>
-        )}
-        <button onClick={onClose}>Close</button>
+        <div className="popup-header">
+          <h2>{categoryName}</h2>
+          <hr className="smooth-line" />
+        </div>
+        <div className="popup-body">
+          {items.length > 0 ? (
+            <ul className="item-list">
+              {items.map((item) => (
+                <li
+                  key={item.item_id}
+                  className="item-container"
+                  onClick={() => addItemToCart(item)}
+                >
+                  <p className="item-name">{item.item_name}</p>
+                  <p className="item-price">€{item.item_price}</p>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No items found for this category.</p>
+          )}
+        </div>
+        <div className="popup-footer">
+          <hr className="smooth-line" />
+          <button onClick={onClose}>Close</button>
+        </div>
       </div>
     </div>
   );
